0e06fdc4a49826935d46ec8df060b9c1e3cc61bc
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-18 21:42:36 -0300

    Initial geolocation and map size

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 import ReactMapGL from "react-map-gl";
 
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const API_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
 export default class Map extends Component {
     state = {
         viewport: {
@@ -9,8 +13,43 @@ export default class Map extends Component {
             height: 400,
             latitude: 37.7577,
             longitude: -122.4376,
-            zoom: 8
+            zoom: 15
         }
+    };
+
+    componentDidMount() {
+        // Set the map's size
+        window.addEventListener("resize", this.resize);
+        this.resize();
+
+        // Get current user's location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.setScreenPositon);
+        }
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize);
+    }
+
+    setScreenPositon = position => {
+        this.setState({
+            viewport: {
+                ...this.state.viewport,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }
+        });
+    };
+
+    resize = () => {
+        this.setState({
+            viewport: {
+                ...this.state.viewport,
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        });
     };
 
     render() {
@@ -18,6 +57,7 @@ export default class Map extends Component {
             <ReactMapGL
                 {...this.state.viewport}
                 onViewportChange={viewport => this.setState({ viewport })}
+                mapboxApiAccessToken={API_TOKEN}
             />
         );
     }
